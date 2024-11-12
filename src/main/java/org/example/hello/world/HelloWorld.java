18b7b9021b611975dbf95be82b898f7ada1be303
Sergey Podkovyrin <podkovyrin.sergey@gmail.com>
2024-11-12 16:30:37 +0300

    java project init

---
+++ src/main/java/org/example/hello/world/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.example.hello.world;
+
+/**
+ *
+ * @author user
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
